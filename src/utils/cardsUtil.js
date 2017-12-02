e0cdac2cb8e1f387e0e889adeea96e568d7cb21f
Alessandro <9150614+Toranius@users.noreply.github.com>
2017-12-02 12:18:10 +0100

    Improve cards initialization

--- src/utils/cardsUtil.js
+++ src/utils/cardsUtil.js
@@ -3,36 +3,24 @@
  */
 export const createRandomList = (images) => {
   const size = 2 * images.cardFrontImages.length;
-
-  let array = createRandomArray(size);
   let availableImages = images.cardFrontImages.slice();
 
-  const cards = new Array(size);
-  for (let i = 0; i < array.length; i += 2) {
+  const cards = [];
+  for (let i = 0; i < size; i += 2) {
     const image = availableImages.pop();
 
-    cards[array[i]] = {
-      id: array[i],
+    cards.push({
+      id: i,
       front: image,
       back: images.cardBackImage
-    };
-
-    cards[array[i + 1]] = {
-      id: array[i + 1],
+    }, {
+      id: i + 1,
       front: image,
       back: images.cardBackImage
-    };
+    });
   }
 
-  return cards;
-}
-
-/**
- * Create an array with integer from 0 to size - 1 shuffled
- */
-function createRandomArray(size) {
-  let array = Array.from(Array(size).keys());
-  return shuffle(array);
+  return shuffle(cards);
 }
 
 /**
